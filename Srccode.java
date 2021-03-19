b8f19762343580952e4d4cc2a94423b04502f669
liang <116996285@qq.com>
2021-03-19 13:01:52 +0800

    v3

--- Srccode.java
+++ Srccode.java
@@ -3,23 +3,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Srccode extends JPanel {
-  protected Instance m_Instance;
-  protected Panel m_BaseInstPanel;
+  protected Instance m_Instance_new;
+  protected Panel m_InstPanel;
     //...
-    public static void setBaseInstance(Instance inst){
+    public static void setInstance(Instance inst){
         m_BaseInstance=inst;
         m_BaseInstPanel.setInstance(m_BaseInstance);
         setBaseInstanceFromFileQ();
         //.......
     }
-    public void setBaseInstanceFromFileQ(){
+    public void setInstanceFromFileQ(){
       int as=0;
       int as2=1;
       Instance i=m_BaseInstance;
       setBaseInstances(new Instances(r));
         //....
     }
-    public void setBaseInstancesFromDBQ(){
+    public void setInstancesFromDBQ(){
       int test=0;
         //......
     }
