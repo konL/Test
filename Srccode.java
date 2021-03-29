4880d89fa5bc8522fa17d72b7f5219b0d229761f
梁嘉慧 <liangjiahui_x@163.com>
2021-03-29 14:59:50 +0800

    v9

--- Srccode.java
+++ Srccode.java
@@ -6,7 +6,7 @@ public class Srccode extends JPanel {
   protected Instance m_Instance_new;
   protected Panel m_InstPanel;
     //...
-    public static void setBaseInstance(Instance inst){
+    public static void setBaseInstance_new(Instance inst){
         m_BaseInstance=inst;
         m_BaseInstPanel.setInstance(m_BaseInstance);
         
