ffca37e90731b62b7b68d46368ac6723c0f877b6
梁嘉慧 <liangjiahui_x@163.com>
2021-03-29 14:50:49 +0800

    v8

--- Ver/new/Srccode.java
+++ Ver/new/Srccode.java
@@ -6,7 +6,7 @@ public class Srccode extends JPanel {
   protected Instance m_Instance_new;
   protected Panel m_InstPanel;
     //...
-    public static void setInstance(Instance inst){
+    public static void setBaseInstance(Instance inst){
         m_BaseInstance=inst;
         m_BaseInstPanel.setInstance(m_BaseInstance);
         
