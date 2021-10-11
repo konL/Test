e30920b75fe2fe1a52a3a1d17555176bfd3d1198
梁嘉慧 <liangjiahui_x@163.com>
2021-10-11 17:02:13 +0800

    1011

--- Srccode.java
+++ Srccode.java
@@ -3,19 +3,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Srccode extends JPanel {
-  protected Instance m_Instance_test;
+  protected Instance m_Instance;
   protected Panel  m_BaseInstPanel;
     //...
     public static void setBaseInstance(Instance inst){
-        m_BaseInstance=inst;
-        m_BaseInstPanel.setInstance(m_BaseInstance);
+      m_Instance=inst;
+        m_BaseInstPanel.setInstance(m_Instance);
         setBaseInstanceFromFileQ();
         //.......
     }
     public void setBaseInstanceFromFileQ(){
       int as=0;
       int as2=1;
-      Instance i=m_BaseInstance;
+      Instance i=m_Instance;
       setBaseInstances(i);
         //....
     }
